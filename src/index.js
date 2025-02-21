0059bf0610a02fe31f73b22b3cfff4b4c3a47f3c
NguyenHuuNhan0212 <nhanvigo@gmail.com>
2025-02-21 20:31:05 +0700

    create: route, controller

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const path = require('path')
 const app = express()
 const port = 3000
 
+const route = require('./routes');
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -22,23 +24,9 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 console.log(__dirname);
-// route
-app.get('/', (req, res) => {
-    res.render('home');
-});
-app.get('/news', (req, res) => {
-    console.log(req.query.q);
-    res.render('news');
-});
-app.get('/search', (req, res) => {
-    res.render('search');
-});
-app.post('/search', (req, res) => {
 
-    console.log(req.body.q);
-
-    res.send(' ');
-});
+// route init
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
