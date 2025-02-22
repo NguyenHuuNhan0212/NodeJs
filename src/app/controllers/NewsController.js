a5cfee119637751e59a43ad5e21db68433a879aa
NguyenHuuNhan0212 <nhanvigo@gmail.com>
2025-02-22 08:36:08 +0700

    install Prettier, Lint-Staged, Husky

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,15 +1,13 @@
-class NewsController{
-
+class NewsController {
     //GET /news
-    index(req, res){
+    index(req, res) {
         res.render('news');
     }
 
     // GET /news/:slug
-    show(req, res){
-        res.send('New detail')
+    show(req, res) {
+        res.send('New detail');
     }
 }
 
-module.exports = new NewsController;
-
+module.exports = new NewsController();
