49753a7d9b2f6f34ba48247355288285ffa73dad
NguyenHuuNhan0212 <nhanvigo@gmail.com>
2025-02-23 13:47:30 +0700

    Chức Năng Thêm Khóa Học

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -11,6 +11,24 @@ class CourseController {
             .catch(next)
       
     }
+    // GET /courses/create
+    create(req, res, next){
+       res.render('courses/create')
+      
+    }
+    //POST /courses/store
+    store(req, res, next){
+        const formData = req.body
+        formData.image = `https://i.ytimg.com/vi/${req.body.videoId}/hqdefault.jpg?sqp=-oaymwEmCMQBEG5IWvKriqkDGQgBFQAAiEIYAdgBAeIBCggYEAIYBjgBQAE=&rs=AOn4CLCa7KMcmhKIoqYKn8qyKPLC3mgm8A`
+        const course = new Course(formData)
+        course.save()
+            .then(() => {
+                res.redirect(`/`)
+            })
+            .catch(error => {
+                
+            })
+     }
    
 }
 
